571855e1fe65776c1d71dd69ab0e4daa08f9d2b0
spiral6 <spiral6@localhost>
2016-09-13 11:11:59 -0500

    Testing some basic GUI programming in JavaFX.

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,18 +1,34 @@
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class MainWindow extends Application {
 
+	static Label text = new Label("This is text.");
+	
 	@Override
 	public void start(Stage primaryStage) {
-		Scene scene = new Scene(new Pane(), 400, 400);
+		
+		Button hello = new Button("Click me!");
+		hello.setOnAction(e -> click());
+		
+		
+		Pane p = new Pane();
+		p.getChildren().add(hello);
+		p.getChildren().add(text);
+		
+		Scene scene = new Scene(p, 400, 400);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Hello world!");
 		primaryStage.show();
-		
+	}
+	
+	public static void click(){
+		text.setText("This is still text.");
 	}
 
 	public static void main(String[] args) {
